8d50d47afa5c0ba497e6f7e1f628abd8fdfb825a
bendzso66 <bendzso66@gmail.com>
2014-12-06 12:13:14 +0100

    [SmartParking] add parking lots generator

---
+++ parking_lots_generator/src/generateParkingLots.java
@@ -0,0 +1,57 @@
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class generateParkingLots {
+
+	static double maxLong = 19.178057;
+	static double minLong = 18.963774;
+	static double rangeLong = maxLong - minLong;
+	static Random randLong = new Random();
+
+	static double maxLat = 47.543692;
+	static double minLat = 47.473005;
+	static double rangeLat = maxLat - minLat;
+	static Random randLat = new Random();
+
+	static Random randIdAndAvailability = new Random();
+
+	static String newLine;
+	static String availability;
+
+	public static void main(String[] args) throws Exception {
+		PrintWriter writer = new PrintWriter("d://insert_random_lines.sql", "UTF-8");
+		for (int i = 0; i < 1000; i++) {
+			double latitude = randLat.nextDouble() * rangeLat + minLat;
+			double longitude = randLong.nextDouble() * rangeLong + minLong;
+			if (randIdAndAvailability.nextBoolean()) {
+				availability = "free";
+			} else {
+				availability = "reserved";
+			}
+			newLine = "INSERT INTO parking_lots (gps_time, latitude, longitude, user_id, parking_lot_availability, address)\nVALUES (" + "'"
+					+ System.currentTimeMillis() + "','" + latitude + "','"
+					+ longitude + "','" + (randIdAndAvailability.nextInt(2000)+1) + "','" + availability + "','"
+					+ "address" + "');";
+			System.out.println(newLine);
+			writer.println(newLine);
+			Thread.sleep(100);
+		}
+		writer.close();
+	}
+
+}
+// CREATE table parking_lots (gps_time LONG, latitude REAL, longitude REAL,
+// user_id LONG, parking_lot_availability VARCHAR(10), address VARCHAR(100));
+// latitude longitude
+// felsõ/alsó bal/jobb
+// 47.469458, 19.053066 bal alsó
+// 47.473656, 18.968753
+
+// 47.468664, 19.064653 jobb alsó
+// 47.473005, 19.178014
+
+// 47.477138, 19.061548 jobb felsõ
+// 47.536349, 19.178057
+
+// 47.476721, 19.050733 bal felsõ
+// 47.543692, 18.963774
