4849aba0aa3505658e944ccd91b168d91c128b94
bendzso66 <bendzso66@gmail.com>
2014-12-06 15:27:05 +0100

    [SmartParking] add interface to sql server

---
+++ interface_to_sql/src/iParkingInterface.java
@@ -0,0 +1,137 @@
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import com.google.gson.Gson;
+
+public class iParkingInterface {
+
+	private static final String dbClassName = "com.mysql.jdbc.Driver";
+	private static final String CONNECTION = "jdbc:mysql://127.0.0.1/smart_parking";
+	private static final int R = 6371; // corrected earth radius, km
+	private static String userName;
+	private static String password;
+
+	public static void main(String[] args) throws ClassNotFoundException,
+			SQLException {
+
+		try {
+			System.out.println("Enter MySQL login name:");
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			userName = in.readLine();
+			System.out.println("Enter password:");
+			in = new BufferedReader(new InputStreamReader(System.in));
+			password = in.readLine();
+		} catch (IOException e) {
+			System.out.println("IOException has been caught" + e);
+		}
+
+		Class.forName(dbClassName);
+		Properties p = new Properties();
+		p.put("user", userName);
+		p.put("password", password);
+
+		final Connection c = DriverManager.getConnection(CONNECTION, p);
+		final Statement stmt = c.createStatement();
+
+		// setPort(5678); //<- Uncomment this if you wan't spark to listen on a
+		// port different than 4567.
+
+		get(new Route("/findFreeLot") {
+
+			@Override
+			public Object handle(Request request, Response response) {
+				try {
+					double radius;
+					try {
+						radius = Double.parseDouble(request.queryParams("rad"));
+					} catch (Exception e) {
+						radius = 1; // 1 km
+					}
+					stmt.execute("SELECT * FROM smart_parking.parking_lots;");
+					ArrayList<rowInParkingLots> lst = getrowsInParkingLots(
+							stmt.getResultSet(),
+							Double.parseDouble(request.queryParams("lat")),
+							Double.parseDouble(request.queryParams("lon")),
+							radius);
+					Gson gson = new Gson();
+					return gson.toJson(lst);
+				} catch (SQLException e) {
+					return "Exception" + e;
+				}
+			}
+		});
+
+		get(new Route("/sendFreeLot") {
+			@Override
+			public Object handle(Request request, Response response) {
+				String answer;
+				try {
+					stmt.execute("INSERT INTO parking_lots (gps_time, latitude, longitude, user_id, parking_lot_availability, address) VALUES (" + "'"
+							+ request.queryParams("time") + "','" + request.queryParams("lat") + "','"
+							+ request.queryParams("lon") + "','" + request.queryParams("user") + "','" + request.queryParams("avail") + "','"
+							+ request.queryParams("addr") + "');");
+					answer = "New row is created.";
+				}catch (SQLException e){
+					response.status(202);
+					answer = "Wrong syntax to create new row. Error message: " + e;
+				}
+				return answer;
+			}
+		});
+
+	}
+
+	private static ArrayList<rowInParkingLots> getrowsInParkingLots(
+			ResultSet rs, double lat1, double lon1, double radius)
+			throws SQLException {
+		ArrayList<rowInParkingLots> lst = new ArrayList();
+		rowInParkingLots row = null;
+
+		Double distance = null;
+		while (rs.next()) {
+			double dLat = Math.toRadians(Double.parseDouble(rs
+					.getString("latitude")) - lat1);
+			double dLon = Math.toRadians(Double.parseDouble(rs
+					.getString("longitude")) - lon1);
+			double a = Math.sin(dLat / 2)
+					* Math.sin(dLat / 2)
+					+ Math.cos(Math.toRadians(lat1))
+					* Math.cos(Math.toRadians(Double.parseDouble(rs
+							.getString("latitude")))) * Math.sin(dLon / 2)
+					* Math.sin(dLon / 2);
+			double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+			distance = R * c;
+			if (distance <= radius
+					& rs.getString("parking_lot_availability").equals("free")) {
+				row = new rowInParkingLots();
+				row.setId(rs.getInt("ID"));
+				row.setGpsTime(rs.getLong("gps_time"));
+				row.setLatitude(rs.getDouble("latitude"));
+				row.setLongitude(rs.getDouble("longitude"));
+				row.setUserId(rs.getLong("user_id"));
+				row.setParkingLotAvailability(rs
+						.getString("parking_lot_availability"));
+				row.setAddress(rs.getString("address"));
+
+				lst.add(row);
+			}
+		}
+		return lst;
+	}
+}
---
+++ interface_to_sql/src/rowInParkingLots.java
@@ -0,0 +1,67 @@
+public class rowInParkingLots {
+
+	private int ID;
+	private long gpsTime;
+	private double latitude;
+	private double longitude;
+	private long userId;
+	private String parkingLotAvailability;
+	private String address;
+
+	public void setId(int ID) {
+		this.ID = ID;
+	}
+
+	public int getId() {
+		return this.ID;
+	}
+
+	public void setGpsTime(long gpsTime) {
+		this.gpsTime = gpsTime;
+	}
+
+	public long getGpsTime() {
+		return this.gpsTime;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLatitude() {
+		return this.latitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLongitude() {
+		return this.longitude;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public long getUserId() {
+		return this.userId;
+	}
+
+	public void setParkingLotAvailability(String parkingLotAvailability) {
+		this.parkingLotAvailability = parkingLotAvailability;
+	}
+
+	public String getParkingLotAvailability() {
+		return this.parkingLotAvailability;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+}
