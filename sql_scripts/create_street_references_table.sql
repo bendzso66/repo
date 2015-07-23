84ffd187045eacc401ec045993e092575dbcb45e
bendzso66 <bendzso66@gmail.com>
2015-07-23 21:27:20 +0200

    SQL: add tables to store datas of parsed osm

---
+++ sql_scripts/create_street_references_table.sql
@@ -0,0 +1,6 @@
+CREATE table street_references (
+	ID INTEGER NOT NULL AUTO_INCREMENT,
+	street_id INTEGER,
+	section_id INTEGER,
+	PRIMARY KEY(ID)
+);
---
+++ sql_scripts/create_street_sections_table.sql
@@ -0,0 +1,8 @@
+CREATE table street_sections (
+	section_id INTEGER NOT NULL,
+	latitude REAL,
+	longitude REAL,
+	changeset INTEGER,
+	parking BOOLEAN DEFAULT 0,
+	PRIMARY KEY(section_id)
+);
