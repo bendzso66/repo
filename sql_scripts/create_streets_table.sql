cfc5f3aaafce93716278e52fc361b2023476759d
bendzso66 <bendzso66@gmail.com>
2015-07-29 23:49:35 +0200

    SQL: add new column to streets table

--- sql_scripts/create_streets_table.sql
+++ sql_scripts/create_streets_table.sql
@@ -2,5 +2,6 @@ CREATE table streets (
     street_id INTEGER NOT NULL,
     changeset INTEGER,
     name_of_street VARCHAR(200),
+    street_of_budapest BOOLEAN DEFAULT 0,
     PRIMARY KEY(street_id)
 );
