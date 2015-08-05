107507709ca5ef260d32f7e51173f341454cea2b
bendzso66 <bendzso66@gmail.com>
2015-08-05 23:07:15 +0200

    JDBC: add connection manager class

---
+++ manage_jdbc_connections/ManageJdbcConnections.java
@@ -0,0 +1,57 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ManageJdbcConnections {
+
+    public static void closeConnections(Connection conn, Statement stmt) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                System.out.println("SQL error: can not close the statement.");
+                e.printStackTrace();
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("SQL error: can not close the connection.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeConnections(Connection conn, Statement stmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                System.out.println("SQL error: can not close the resultset.");
+                e.printStackTrace();
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                System.out.println("SQL error: can not close the statement.");
+                e.printStackTrace();
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("SQL error: can not close the connection.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
