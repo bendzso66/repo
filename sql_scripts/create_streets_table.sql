e700fc2562512aadd719cce089ec82056b0b0c72
bendzso66 <bendzso66@gmail.com>
2015-08-08 00:08:28 +0200

    SQL: delete default value of street_of_budapest in streets table

--- sql_scripts/create_streets_table.sql
+++ sql_scripts/create_streets_table.sql
@@ -1,6 +1,6 @@
 CREATE table streets (
     street_id INTEGER NOT NULL,
     name_of_street VARCHAR(200),
-    street_of_budapest BOOLEAN DEFAULT 0,
+    street_of_budapest BOOLEAN,
     PRIMARY KEY(street_id)
 );
