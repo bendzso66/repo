970a96ed218513315d5a24db4cadd23c086dce31
bendzso66 <bendzso66@gmail.com>
2015-08-08 00:06:20 +0200

    SQL: delete changeset rows

--- sql_scripts/create_street_sections_table.sql
+++ sql_scripts/create_street_sections_table.sql
@@ -2,7 +2,6 @@ CREATE table street_sections (
     section_id INTEGER NOT NULL,
     latitude REAL,
     longitude REAL,
-    changeset INTEGER,
     parking BOOLEAN DEFAULT 0,
     PRIMARY KEY(section_id)
 );
