5fb9f95b7f94aa44475465605f5001efd58b84e5
bendzso66 <bendzso66@gmail.com>
2015-10-13 21:51:10 +0200

    Servlet: fix runSmartParkingServlet.sh

--- runSmartParkingServlet.sh
+++ runSmartParkingServlet.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-java -cp .:jetty-webapp-7.3.0.v20110203.jar                     \
-          :log4j-1.2.14.jar:mysql-connector-java-5.1.34-bin.jar \
-          :servlet-api-3.0.pre4.jar                             \
-          :slf4j-api-1.6.1.jar                                  \
-          :slf4j-log4j12-1.6.1.jar                              \
-          :spark-0.9.9.4-SNAPSHOT.jar                           \
-          :spark-0.9.9.4-SNAPSHOT-javadoc.jar                   \
-          :spark-0.9.9.4-SNAPSHOT-sources.jar                   \
-          :gson-2.2.4.jar:gson-2.2.4.jar                        \
-          :org.json-20120521.jar                                \
-    SmartParkingServlet
+java -cp .:res/jar_files/gson/gson-2.2.4.jar:res/jar_files/jdbc_connector/mysql-connector-java-5.1.34-bin.jar:res/jar_files/json/org.json-20120521.jar:res/jar_files/spark/jetty-webapp-7.3.0.v20110203.jar:res/jar_files/spark/log4j-1.2.14.jar:res/jar_files/spark/servlet-api-3.0.pre4.jar:res/jar_files/spark/slf4j-api-1.6.1.jar:res/jar_files/spark/slf4j-log4j12-1.6.1.jar:res/jar_files/spark/spark-0.9.9.4-SNAPSHOT.jar:res/jar_files/spark/spark-0.9.9.4-SNAPSHOT-javadoc.jar:res/jar_files/spark/spark-0.9.9.4-SNAPSHOT-sources.jar hu.bme.hit.smartparking.servlet.SmartParkingServlet
