c5b431cc9166cfdbc94d40ab621afe055198df9e
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-03-19 21:31:55 +0100

    SQL: remove unused column from streets table

--- sql_scripts/create_streets_table.sql
+++ sql_scripts/create_streets_table.sql
@@ -1,7 +1,6 @@
 CREATE table streets (
     street_id BIGINT(20) NOT NULL,
     name_of_street VARCHAR(200),
-    street_of_budapest BOOLEAN,
     PRIMARY KEY(street_id)
 )
 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
