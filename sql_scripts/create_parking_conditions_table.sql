fa4bf2857eb561b21c96a23442ed097e111708f8
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-03-31 00:01:59 +0200

    SQL: rename fields in parking_conditions table

--- sql_scripts/create_parking_conditions_table.sql
+++ sql_scripts/create_parking_conditions_table.sql
@@ -1,8 +1,8 @@
 CREATE table parking_conditions (
     ID BIGINT(20) NOT NULL AUTO_INCREMENT,
     street_id BIGINT(20) NOT NULL,
-    parking_condition_key VARCHAR(20),
-    parking_condition_value VARCHAR(20),
+    side VARCHAR(20),
+    parking_condition VARCHAR(20),
     PRIMARY KEY(ID)
 )
 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
