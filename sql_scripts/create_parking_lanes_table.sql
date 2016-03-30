28564e4cfbc6ab75fe964d168fc5b951e455b636
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-03-30 23:28:27 +0200

    SQL: rename fields in parking_lanes table

--- sql_scripts/create_parking_lanes_table.sql
+++ sql_scripts/create_parking_lanes_table.sql
@@ -1,8 +1,8 @@
 CREATE table parking_lanes (
     ID BIGINT(20) NOT NULL AUTO_INCREMENT,
     street_id BIGINT(20) NOT NULL,
-    parking_lane_key VARCHAR(20),
-    parking_lane_value VARCHAR(20),
+    side VARCHAR(20),
+    direction VARCHAR(20),
     PRIMARY KEY(ID)
 )
 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
