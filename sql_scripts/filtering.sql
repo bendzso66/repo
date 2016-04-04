f5eb2d3ad69148048e2550472c3920ec4e4ac781
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-04-04 21:21:43 +0200

    SQL: filter by highway type instead of not null fields and make some refact

--- sql_scripts/filtering.sql
+++ sql_scripts/filtering.sql
@@ -14,25 +14,38 @@ WHERE street_id = ANY (SELECT street_id
 
 -- filter nodes in a square around Budapest
 CREATE TABLE vehicle_data.budapest_nodes LIKE vehicle_data.nodes;
-insert vehicle_data.budapest_nodes SELECT *
-												  FROM vehicle_data.nodes
-												  WHERE latitude < 47.623717
-												  AND latitude > 47.351792
-												  AND longitude < 19.358956
-												  AND longitude > 18.936853;
+INSERT vehicle_data.budapest_nodes SELECT * FROM vehicle_data.nodes
+											WHERE latitude < 47.623717
+											AND latitude > 47.351792
+											AND longitude < 19.358956
+											AND longitude > 18.936853;
                                                   
 -- filter street_references in a square around Budapest
 CREATE TABLE vehicle_data.budapest_street_references LIKE vehicle_data.street_references;
-insert vehicle_data.budapest_street_references select * from vehicle_data.street_references where node_id = any (select node_id from vehicle_data.budapest_nodes);
+INSERT vehicle_data.budapest_street_references SELECT * FROM vehicle_data.street_references
+WHERE node_id = ANY (SELECT node_id FROM vehicle_data.budapest_nodes);
 
 -- filter streets in a square around Budapest
 CREATE TABLE vehicle_data.budapest_streets LIKE vehicle_data.streets;
-insert vehicle_data.budapest_streets select * from vehicle_data.streets where street_id = any(select street_id from vehicle_data.budapest_street_references);
+INSERT vehicle_data.budapest_streets SELECT * FROM vehicle_data.streets
+WHERE street_id = ANY (SELECT street_id FROM vehicle_data.budapest_street_references);
 
--- fill not null tables
-CREATE TABLE vehicle_data.budapest_streets_not_null LIKE vehicle_data.streets;
-insert vehicle_data.budapest_streets_not_null select * from vehicle_data.budapest_streets where name_of_street is not null;
-CREATE TABLE vehicle_data.budapest_street_references_not_null LIKE vehicle_data.street_references;
-insert vehicle_data.budapest_street_references_not_null (select * from vehicle_data.budapest_street_references where street_id = any (select street_id from vehicle_data.budapest_streets_not_null));
-CREATE TABLE vehicle_data.budapest_nodes_not_null LIKE vehicle_data.nodes;
-insert vehicle_data.budapest_nodes_not_null (select * from vehicle_data.budapest_nodes where node_id = any (select node_id from vehicle_data.budapest_street_references_not_null));
+-- delete streets, street references and nodes which are not related to these types of ways:
+-- secondary, tertiary, unclassified, residential, service or living_street ways
+DELETE FROM vehicle_data.budapest_streets
+WHERE (highway <> "secondary"
+       AND highway <> "tertiary"
+       AND highway <> "unclassified"
+       AND highway <> "residential"
+       AND highway <> "service"
+       AND highway <> "living_street")
+      OR highway IS NULL;
+
+DELETE FROM vehicle_data.budapest_street_references
+WHERE vehicle_data.budapest_street_references.street_id NOT IN (SELECT street_id FROM vehicle_data.budapest_streets);
+
+CREATE TABLE vehicle_data.budapest_nodes_tmp LIKE vehicle_data.budapest_nodes;
+INSERT vehicle_data.budapest_nodes_tmp (SELECT * FROM vehicle_data.budapest_nodes
+WHERE node_id = ANY (SELECT node_id FROM vehicle_data.budapest_street_references));
+DROP TABLE budapest_nodes;
+RENAME TABLE budapest_nodes_tmp TO budapest_nodes;
