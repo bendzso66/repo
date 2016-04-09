0440aed53f2b5d19fbd530155c80380dbefa5fcf
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-04-09 18:08:42 +0200

    SQL: add length_of_way column to ways table

--- sql_scripts/create_ways_table.sql
+++ sql_scripts/create_ways_table.sql
@@ -2,6 +2,7 @@ CREATE table ways (
     way_id BIGINT(20) NOT NULL,
     name_of_way VARCHAR(200),
     highway VARCHAR(30),
+    length_of_way REAL,
     PRIMARY KEY(way_id)
 )
 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
