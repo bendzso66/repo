749e955a7d4727874500462ba948846f9248f9fa
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-04-10 15:00:15 +0200

    SQL: add latitude and longitude columns to ways table

--- sql_scripts/create_ways_table.sql
+++ sql_scripts/create_ways_table.sql
@@ -2,6 +2,8 @@ CREATE table ways (
     way_id BIGINT(20) NOT NULL,
     name_of_way VARCHAR(200),
     highway VARCHAR(30),
+    latitude REAL,
+    longitude REAL,
     length_of_way REAL,
     PRIMARY KEY(way_id)
 )
