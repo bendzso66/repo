ee64873c92359b0fab28fc8786ff97131c3bcd6e
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-04-10 14:48:27 +0200

    ParseMap: write the center coords of ways to the ways table

--- src/hu/bme/hit/smartparking/parsemap/CreateWaySections.java
+++ src/hu/bme/hit/smartparking/parsemap/CreateWaySections.java
@@ -42,7 +42,8 @@ public class CreateWaySections {
     private static final String WAY_SECTIONS_TABLE = "vehicle_data.budapest_way_sections ";
     private static final String WAY_SECTIONS_TABLE_HEADERS = "(way_id,node_id_1,latitude_1,longitude_1,node_id_2,latitude_2,longitude_2,length_of_section) ";
     private static final String JOIN_CONDITION = "vehicle_data.budapest_way_references.node_id=vehicle_data.budapest_nodes.node_id ";
-    private static final String WAY_ID_EQUALS = "way_id=";
+    private static final String WAY_ID = "way_id";
+    private static final String WAY_ID_EQUALS = WAY_ID + "=";
     private static final String NODE_ID = "node_id";
     private static final String LATITUDE = "latitude";
     private static final String LONGITUDE = "longitude";
@@ -52,11 +53,18 @@ public class CreateWaySections {
     private static final String QUOTATION_MARK = "'";
     private static final String QUOTATION_MARKS_WITH_COMMA = "','";
     private static final String CLOSING_BRACKET = "');";
+    private static final String UPDATE_BUDAPEST_WAYS = "UPDATE vehicle_data.budapest_ways ";
+    private static final String SET_LATITUDE_EQUALS = "SET latitude=";
+    private static final String COMMA = ", ";
+    private static final String LONGITUDE_EQUALS = "longitude=";
+    private static final String WHERE_WAY_ID_EQUALS = "WHERE " + WAY_ID_EQUALS;
 
     private static final String CONNECTION_ERROR = "SQL error: cannot create the connection.";
     private static final String SQL_QUERY_ERROR = "SQL error: cannot execute query.";
     private static final String SQL_CONNECTIONS_ARE_CLOSED = "SQL connections are closed!";
     private static final String WAY_SECTIONS_NEW_RECORD_ERROR = "SQL error: cannot create new record in table budapest_way_sections.";
+    private static final String WAY_REFERENCES_QUERY_ERROR = "SQL error: query from budapest_ways was unsuccessful.";
+    private static final String WAYS_UPDATE_ERROR = "SQL error: update in budapest_ways was unsuccessful.";
 
     public static void main(String argv[]) throws ClassNotFoundException {
 
@@ -83,7 +91,7 @@ public class CreateWaySections {
 
             int wayCounter = 1;
             while (waysResultSet.next()) {
-                long wayId = waysResultSet.getLong("way_id");
+                long wayId = waysResultSet.getLong(WAY_ID);
 
                 String sqlQueryFromWayReferencesTable = SELECT
                         + REQUESTED_FIELDS
@@ -99,10 +107,9 @@ public class CreateWaySections {
                         + ORDER_BY
                         + ID
                         + SEMICOLON;
-                String sqlQueryFromWayReferencesTableErrorMsg = "SQL error: query from budapest_ways was unsuccessful.";
                 nodesResultSet = CommonJdbcMethods.executeQueryStatement(
                         nodesStatement, sqlQueryFromWayReferencesTable,
-                        sqlQueryFromWayReferencesTableErrorMsg);
+                        WAY_REFERENCES_QUERY_ERROR);
 
                 ArrayList<Node> nodes = new ArrayList<>();
                 while (nodesResultSet.next()) {
@@ -111,7 +118,24 @@ public class CreateWaySections {
                                     .getDouble(LONGITUDE)));
                 }
 
-                int numOfSections = nodes.size() - 1;
+                int numOfNodes = nodes.size();
+                if (numOfNodes > 0) {
+                    int middleNodePos = (int) Math.floor(numOfNodes / 2);
+                    String sqlUpdateStatement = UPDATE_BUDAPEST_WAYS
+                            + SET_LATITUDE_EQUALS
+                            + nodes.get(middleNodePos).getLatitude()
+                            + COMMA
+                            + LONGITUDE_EQUALS
+                            + nodes.get(middleNodePos).getLongitude()
+                            + WHERE_WAY_ID_EQUALS
+                            + wayId
+                            + SEMICOLON;
+                    CommonJdbcMethods.executeUpdateStatement(waySectionsStatement,
+                            sqlUpdateStatement,
+                            WAYS_UPDATE_ERROR);
+                }
+
+                int numOfSections = numOfNodes - 1;
                 for (int i = 0; i < numOfSections; i++) {
                     Node startNode = nodes.get(i);
                     Node endNode = nodes.get(i + 1);
