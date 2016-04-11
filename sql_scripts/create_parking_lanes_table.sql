b9d75b5f65b6dffeb959cd7b900978c92c52a7b4
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-04-11 22:12:41 +0200

    SQL: add from_user column to parking_lanes and parking_conditions table

--- sql_scripts/create_parking_lanes_table.sql
+++ sql_scripts/create_parking_lanes_table.sql
@@ -3,6 +3,7 @@ CREATE table parking_lanes (
     way_id BIGINT(20) NOT NULL,
     side VARCHAR(30),
     direction VARCHAR(20),
+    from_user TINYINT(1),
     PRIMARY KEY(ID)
 )
 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
