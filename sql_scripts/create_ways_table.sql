2bdb1cce64926f6d823371e80e1f872e2f525562
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-04-12 19:16:12 +0200

    SQL: add end coords to ways table and rename center coords

--- sql_scripts/create_ways_table.sql
+++ sql_scripts/create_ways_table.sql
@@ -2,8 +2,12 @@ CREATE table ways (
     way_id BIGINT(20) NOT NULL,
     name_of_way VARCHAR(200),
     highway VARCHAR(30),
-    latitude REAL,
-    longitude REAL,
+    center_latitude REAL,
+    center_longitude REAL,
+    latitude_1 REAL,
+    longitude_1 REAL,
+    latitude_2 REAL,
+    longitude_2 REAL,
     length_of_way REAL,
     PRIMARY KEY(way_id)
 )
