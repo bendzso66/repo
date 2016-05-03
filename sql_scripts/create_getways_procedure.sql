e5fb6bc964f75c71710cd5c0ca6e9e4c75b09fb6
benjaminbosko55 <benjamin.bosko55@gmail.com>
2016-05-03 10:54:55 +0200

    SQL: get the union of endcoords instead of the center coords

--- sql_scripts/create_getways_procedure.sql
+++ sql_scripts/create_getways_procedure.sql
@@ -18,18 +18,33 @@ BEGIN
 
 SELECT way_id, name_of_way, latitude_1, longitude_1, latitude_2,  longitude_2, all_spaces, free_spaces,
     ( ( 2 * 6371
-    * asin( sqrt( sin( radians( mylat - center_latitude ) / 2 )
-                * sin( radians( mylat - center_latitude ) / 2 )
+    * asin( sqrt( sin( radians( mylat - latitude_1 ) / 2 )
+                * sin( radians( mylat - latitude_1 ) / 2 )
                 + cos( radians( mylat ) )
-                * cos( radians( center_latitude ) )
-                * sin( radians( mylng - center_longitude ) / 2 )
-                * sin( radians( mylng - center_longitude ) / 2 ) ) ) )
+                * cos( radians( latitude_1 ) )
+                * sin( radians( mylng - longitude_1 ) / 2 )
+                * sin( radians( mylng - longitude_1 ) / 2 ) ) ) )
     * 1000 ) AS distance
     FROM vehicle_data.budapest_ways
     WHERE budapest_ways.all_spaces > 0
-    AND (budapest_ways.center_latitude BETWEEN lat1 AND lat2)
-    AND (budapest_ways.center_longitude BETWEEN lng1 AND lng2)
+    AND (budapest_ways.latitude_1 BETWEEN lat1 AND lat2)
+    AND (budapest_ways.longitude_1 BETWEEN lng1 AND lng2)
     HAVING distance < dist
-    ORDER BY distance;
+UNION
+SELECT way_id, name_of_way, latitude_1, longitude_1, latitude_2,  longitude_2, all_spaces, free_spaces,
+    ( ( 2 * 6371
+    * asin( sqrt( sin( radians( mylat - latitude_2 ) / 2 )
+                * sin( radians( mylat - latitude_2 ) / 2 )
+                + cos( radians( mylat ) )
+                * cos( radians( latitude_2 ) )
+                * sin( radians( mylng - longitude_2 ) / 2 )
+                * sin( radians( mylng - longitude_2 ) / 2 ) ) ) )
+    * 1000 ) AS distance
+    FROM vehicle_data.budapest_ways
+    WHERE budapest_ways.all_spaces > 0
+    AND (budapest_ways.latitude_2 BETWEEN lat1 AND lat2)
+    AND (budapest_ways.longitude_2 BETWEEN lng1 AND lng2)
+    HAVING distance < dist
+ORDER BY distance;
 END $$
 DELIMITER ;
